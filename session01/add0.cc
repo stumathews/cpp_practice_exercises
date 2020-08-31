5a0f656288897994581c632b61a29669a601cb6d
Stuart Mathews <stumathews@gmail.com>
2020-08-31 13:25:34 +0100

    use add_by_1 to perform multiplication

--- session01/add0.cc
+++ session01/add0.cc
@@ -7,17 +7,30 @@
 using namespace std;
 
 auto add_by_1 = []( int a, int b ) {
-	// Keep adding 1 to a until b becomes 0.
+	while(b-- > 0)
+		a += 1;
+	return a;
+};
+
+auto mult = []( int a, int b ) {
+	int z = 0;
+	auto end = a - 1;
+	while(a >= end)
+		z = add_by_1(a-- * z,b);
+	return z;
 };
 
 int main() {
 	int i, j;
 
-	while (cin >> i >> j && (i != 0 || j != 0))
+	while (cin >> i >> j && (i != 0 || j != 0)){
 		cout << i << " + " << j << " = " << add_by_1(i, j) << endl;
+		cout << i << " * " << j << " = " << mult(i, j) << endl;
+	}
 
 	i = 0; j = 0;
 	cout << i << " + " << j << " = " << add_by_1(i, j) << endl;
+	cout << i << " * " << j << " = " << mult(i, j) << endl;
 
 	return 0;
 }
