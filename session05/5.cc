3944df8bde4e1a991df14a1e004b3ec322e763a8
Stuart Mathews <stumathews@gmail.com>
2020-09-01 22:44:55 +0100

    spell 'occurred' correctly

--- session05/5.cc
+++ session05/5.cc
@@ -8,12 +8,14 @@ int main(int argc, char **argv)
 {
 	string word;
 	map<string,int> occurs;
+	
 	while(cin >> word) {
 		occurs[word]++;
 		cout << word << endl;
 	}
+
 	for(auto b = begin(occurs); b != end(occurs);b++){
-		cout << b->first << " occured " << b->second << " times." << endl;
+		cout << b->first << " occurred " << b->second << " times." << endl;
 	}
 	return 0;
 }
