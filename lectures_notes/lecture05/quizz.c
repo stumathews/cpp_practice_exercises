a8e9bd847806ebb838df8ba9422ca54f01f523cc
Stuart Mathews <stumathews@gmail.com>
2020-10-29 19:34:16 +0000

    notes

---
+++ lectures_notes/lecture05/quizz.c
@@ -0,0 +1,9 @@
+int **p1 // p1 is a pointer to pointer to an int
+const int **p2 // p2 is a pointer to a pointer of constant int
+int * const *p3 // p3 is a pointer to a const pointer to a pointer to an int
+int * * const p4 // p4 is a const pointer to a pointer to a pointer of int
+const int * const *p5 // p5 is a pointer to a constant pointer to an const int
+const int * * const p6 // p6 is a constan pointer to a pointer to a const int
+int * const * const p7 // p7 is a const pointer to constant pointer to an int
+const int * const * const p8 // p8 is a constant opinter to a constant pointer to a const int
+
